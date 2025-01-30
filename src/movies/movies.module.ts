54133918c7ff83041a1aeb37156e58959e93bfd1
ramvignesh-b <ramvignesh@incubyte.co>
2025-01-30 16:40:42 -0500

    feat: movie service invokes httpservice and returns NEW

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
-
+import { HttpModule } from '@nestjs/axios';
 @Module({
+  imports: [HttpModule],
   controllers: [MoviesController],
   providers: [MoviesService]
 })
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getOldness(movieName: string): Promise<string> {
+    const response = await firstValueFrom(this.httpService.get(`http://localhost:3002/${movieName}`));
+    return 'NEW';
+  }
+}
