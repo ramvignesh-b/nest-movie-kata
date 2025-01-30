d43e9fc8bde0cd647c60598320592cbd45554375
ramvignesh-b <ramvignesh@incubyte.co>
2025-01-30 16:23:45 -0500

    feat: minimal controller to return OLD

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get(':movieName/oldness')
+  async getOldness(@Param('movieName') movieName: string) {
+    const oldness = await this.moviesService.getOldness(movieName);
+    return { category: oldness };
+  }
+}
