7e685ddd358778c98a3ce99cee719e51c47bd43c
ramvignesh-b <ramvignesh@incubyte.co>
2025-01-30 16:09:09 -0500

    chore: init test configurations and structure

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.spec.ts'],
+    globals: true,
+    root: './',
+  },
+  plugins: [swc.vite()],
+});
